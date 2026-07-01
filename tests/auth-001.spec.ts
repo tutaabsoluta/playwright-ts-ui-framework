d770b8af57ee107616f144dc155636465db4ed73
Sergio Duran <538ser@gmail.com>
2026-07-01 17:54:28 -0600

    code refactor

--- tests/auth-001.spec.ts
+++ tests/auth-001.spec.ts
@@ -1,8 +1,8 @@
 import test, { expect } from "@playwright/test";
 
 
-test.describe('Should Register a new User. Verify that a new user can successfully register', () => {
-    test('Should do auth flow',async ({ page }) => {
+test.describe('User registration', () => {
+    test('Should register a new user successfully',async ({ page }) => {
         await page.goto('https://automationexercise.com/')
         const titleLocator = await page.locator('.logo.pull-left');
         await expect(titleLocator).toBeVisible();
@@ -12,16 +12,18 @@ test.describe('Should Register a new User. Verify that a new user can successful
         await signUpLink.click();
 
         // register flow
-        const signupNameTextbox = await page.locator('[data-qa="signup-name"]');
+        const signupNameTextbox = page.locator('[data-qa="signup-name"]');
         await signupNameTextbox.fill('Sergio');
 
-        const signupEmailAddress = await page.locator('[data-qa="signup-email"]');
-        await signupEmailAddress.fill('sergio@mail.com')
+        const emailInput = page.locator('[data-qa="signup-email"]');
+        await emailInput.fill('sergio@mail.com')
 
-        const signUpCta = await page.locator('[data-qa="signup-button"]');
-        await signUpCta.click();
+        const signUpButton = page.locator('[data-qa="signup-button"]');
+        await signUpButton.click();
         
-        const signUpPageHeader = await page.getByText('Enter Account Information')
+        const signUpPageHeader = page.getByText('Enter Account Information')
         await expect(signUpPageHeader).toBeVisible();
+
+
     })
 })
