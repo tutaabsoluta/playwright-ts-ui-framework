fe8a1125ae4eb1aea18047358360b7e5a4f63d4c
Sergio Duran <538ser@gmail.com>
2026-08-23 15:47:38 -0600

    add assertions to logged in test

--- tests/auth/auth-002.spec.ts
+++ tests/auth/auth-002.spec.ts
@@ -1,17 +1,25 @@
-import test from "@playwright/test";
+import { test } from "../../fixtures/existingUser.fixture";
+
 import { env } from '../../env'
+import { expect } from "@playwright/test";
 
 test.describe('Login flow', () => {
-    test('Should login successfully with valid credentials', async ({ page }) => {
+    test('Should login successfully with valid credentials', async ({ page, testUser }) => {
         await page.goto('https://automationexercise.com/');
         const signUpLink = await page.locator('[href="/login"]');
         await signUpLink.click();
 
         // login
         const emailInput = page.locator('[data-qa="login-email"]');
-        await emailInput.fill(env.USER_MAIL)
+        await emailInput.fill(testUser.email)
 
         const passwordInput = page.locator('[data-qa="login-password"]');
-        await passwordInput.fill(env.USER_PASSWORD)
+        await passwordInput.fill(testUser.password)
+
+        const loginCta = page.locator('[data-qa="login-button"]')
+        await loginCta.click();
+
+        const loggedInAsLink = page.locator('.fa.fa-user');
+        await expect(loggedInAsLink).toBeVisible();
     })
 })
