3867f94e4910ad6f6e299ca8ec1b39cb9e965853
Sergio Duran <538ser@gmail.com>
2026-08-28 16:07:00 -0600

    Add login code and product nav link selector

--- tests/products/products-001.spec.ts
+++ tests/products/products-001.spec.ts
@@ -1,7 +1,22 @@
-import { test } from "@playwright/test";
+import { test } from "../../fixtures/existingUser.fixture";
+import { LoginPage } from "../../pages/LoginPage";
+
 
 test.describe("Products", () => {
-    test("Should view all products", async ({ page }) => {
+    test("Should view all products", async ({ page, testUser }) => {
 
+        const loginPage = new LoginPage(page)
+        await page.goto('/')
+
+        // Navigate to login page
+        await loginPage.clickSignUpLink();
+
+        // Login
+        await loginPage.fillLoginEmail(testUser.email);
+        await loginPage.fillLoginPassword(testUser.password);
+        await loginPage.clickLoginButton();
+        
+        const productsNavLink = page.locator('[href="/products"]')
+        await productsNavLink.click()
     });
 });
