48d2c4f35eb6e1483a651a8b3c183fb5f59fa4a0
Sergio Duran <538ser@gmail.com>
2026-08-31 17:10:30 -0600

    add assertions for product details script

--- tests/products/products-002.spec.ts
+++ tests/products/products-002.spec.ts
@@ -1,7 +1,32 @@
-import { test } from "@playwright/test";
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/existingUser.fixture";
+import { LoginPage } from "../../pages/LoginPage";
 
 test.describe("Products", () => {
-    test("Should view product details", async ({ page }) => {
+    test("Should view product details", async ({ page, testUser }) => {
+        await page.goto('/')
+        const loginPage = new LoginPage(page)
+        // Navigate to login page
+        await loginPage.clickSignUpLink();
+
+        // Login
+        await loginPage.fillLoginEmail(testUser.email);
+        await loginPage.fillLoginPassword(testUser.password);
+        await loginPage.clickLoginButton();
+
+        const productsNavLink = page.locator('[href="/products"]')
+        await productsNavLink.click()
+
+        const viewProductCta = page.locator('[href="/product_details/1"]')
+        await viewProductCta.click()
+
+        // product details
+        const productDetailsContainer = page.locator('.product-details')
+        await expect(productDetailsContainer.locator('h2')).toHaveText('Blue Top')
+        await expect(productDetailsContainer.locator('p').first()).toHaveText('Category: Women > Tops')
+        await expect(productDetailsContainer.locator('span').locator('span')).toHaveText('Rs. 500')
+        // await expect(productDetailsContainer.locator('p').nth(1).locator('b')).toHaveText('Availability:')
+        await expect(productDetailsContainer.locator('p').nth(1)).toHaveText('Availability: In Stock')
 
     });
 });
