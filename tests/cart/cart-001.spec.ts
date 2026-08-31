174c825aaf6c656a1f56f194c4776a4c431b730f
Sergio Duran <538ser@gmail.com>
2026-08-31 17:35:43 -0600

    add three script files for cart validations

---
+++ tests/cart/cart-001.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/existingUser.fixture";
+import { LoginPage } from "../../pages/LoginPage";
+
+test.describe("Cart", () => {
+    test("Should add a product to cart", async ({ page, testUser }) => {
+        
+    });
+});
