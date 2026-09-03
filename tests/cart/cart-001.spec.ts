be6efb0e585c30e7ac972c3885e25dd6d6c0a6d0
Sergio Duran <538ser@gmail.com>
2026-09-02 18:35:46 -0600

    add cart selection to first cart script

--- tests/cart/cart-001.spec.ts
+++ tests/cart/cart-001.spec.ts
@@ -7,5 +7,24 @@ test.describe("Cart", () => {
         const loginPage = new LoginPage(page)
 
         await page.goto('/')
+
+        // Navigate to login page
+        await loginPage.clickSignUpLink();
+
+        // Login
+        await loginPage.fillLoginEmail(testUser.email);
+        await loginPage.fillLoginPassword(testUser.password);
+        await loginPage.clickLoginButton();
+
+        const productsNavLink = page.locator('[href="/products"]')
+        await productsNavLink.click()
+
+        const addToCartCta = page.locator('.btn.btn-default.add-to-cart')
+        await addToCartCta.first().click()
+
+        const continueShoppingCta = page.locator('.btn.btn-success.close-modal.btn-block')
+        await continueShoppingCta.click()
+        const cartNavLink = page.getByRole('link', { name: ' Cart' })
+        await cartNavLink.click()
     });
 });
