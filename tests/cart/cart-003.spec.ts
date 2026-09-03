3ad279c4162f316a1734c7f810083e8389868ecf
Sergio Duran <538ser@gmail.com>
2026-09-03 17:01:18 -0600

    add code to cart 2 and 3 spec files

--- tests/cart/cart-003.spec.ts
+++ tests/cart/cart-003.spec.ts
@@ -4,6 +4,19 @@ import { LoginPage } from "../../pages/LoginPage";
 
 test.describe("Cart", () => {
     test("Should update product quantity", async ({ page, testUser }) => {
-        
+        const loginPage = new LoginPage(page)
+
+        await page.goto('/')
+
+        // Navigate to login page
+        await loginPage.clickSignUpLink();
+
+        // Login
+        await loginPage.fillLoginEmail(testUser.email);
+        await loginPage.fillLoginPassword(testUser.password);
+        await loginPage.clickLoginButton();
+
+        const productsNavLink = page.locator('[href="/products"]')
+        await productsNavLink.click()
     });
 });
